b26aad8ccf133a8723899f2ba61703515adeaa45
“Austin <austinkloske@gmail.com>
2022-01-25 11:45:23 +0100

    sqlite deployment

--- gen/srv/srv/server.js
+++ gen/srv/srv/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const cds = require('@sap/cds');
+
 const cfenv = require('cfenv');
 const appEnv = cfenv.getAppEnv();
 const xsenv = require('@sap/xsenv');
@@ -20,100 +22,106 @@ app.use(passport.authenticate('JWT', {
     session: false
 }));
 
-app.use(bodyParser.json());
+cds.on('bootstrap', (app) => {
 
-const lib = require('./library');
+    app.use(bodyParser.json());
+    const lib = require('./library');
 
-
-
-// subscribe/onboard a subscriber tenant
-app.put('/callback/v1.0/tenants/*', function (req, res) {
-    let tenantHost = req.body.subscribedSubdomain + '-' + appEnv.app.space_name.toLowerCase().replace(/_/g, '-') + '-' + services.registry.appName.toLowerCase().replace(/_/g, '-');
-    let tenantURL = 'https:\/\/' + tenantHost + /\.(.*)/gm.exec(appEnv.app.application_uris[0])[0];
-    console.log('Subscribe: ', req.body.subscribedSubdomain, req.body.subscribedTenantId, tenantHost, tenantURL);
-    lib.createRoute(tenantHost, services.registry.appName).then(
-        function (result) {
-            res.status(200).send(tenantURL);
-        },
-        function (err) {
-            console.log(err.stack);
-            res.status(500).send(err.message);
-        });
-});
-
-// unsubscribe/offboard a subscriber tenant
-app.delete('/callback/v1.0/tenants/*', function (req, res) {
-    let tenantHost = req.body.subscribedSubdomain + '-' + appEnv.app.space_name.toLowerCase().replace(/_/g, '-') + '-' + services.registry.appName.toLowerCase().replace(/_/g, '-');
-    console.log('Unsubscribe: ', req.body.subscribedSubdomain, req.body.subscribedTenantId, tenantHost);
-    lib.deleteRoute(tenantHost, services.registry.appName).then(
-        function (result) {
-            res.status(200).send('');
-        },
-        function (err) {
-            console.log(err.stack);
-            res.status(500).send(err.message);
-        });
-});
-
-// get reuse service dependencies
-app.get('/callback/v1.0/dependencies', function (req, res) {
-    let tenantId = req.params.tenantId;
-    let dependencies = [{
-        'xsappname': services.dest.xsappname
-    }];
-    console.log('Dependencies: ', tenantId, dependencies);
-    res.status(200).json(dependencies);
-});
-
-// app user info
-app.get('/srv/info', function (req, res) {
-    if (req.authInfo.checkScope('$XSAPPNAME.User')) {
-        let info = {
-            'userInfo': req.user,
-            'subdomain': req.authInfo.getSubdomain(),
-            'tenantId': req.authInfo.getZoneId()
-        };
-        res.status(200).json(info);
-    } else {
-        res.status(403).send('Forbidden');
-    }
-});
-
-// app subscriptions
-app.get('/srv/subscriptions', function (req, res) {
-    if (req.authInfo.checkScope('$XSAPPNAME.Administrator')) {
-        lib.getSubscriptions(services.registry).then(
+    // subscribe/onboard a subscriber tenant
+    app.put('/callback/v1.0/tenants/*', function (req, res) {
+        let tenantHost = req.body.subscribedSubdomain + '-' + appEnv.app.space_name.toLowerCase().replace(/_/g, '-') + '-' + services.registry.appName.toLowerCase().replace(/_/g, '-');
+        let tenantURL = 'https:\/\/' + tenantHost + /\.(.*)/gm.exec(appEnv.app.application_uris[0])[0];
+        console.log('Subscribe: ', req.body.subscribedSubdomain, req.body.subscribedTenantId, tenantHost, tenantURL);
+        lib.createRoute(tenantHost, services.registry.appName).then(
             function (result) {
-                res.status(200).json(result);
+                res.status(200).send(tenantURL);
             },
             function (err) {
                 console.log(err.stack);
                 res.status(500).send(err.message);
             });
-    } else {
-        res.status(403).send('Forbidden');
-    }
-});
+    });
 
-
-// destination reuse service
-app.get('/srv/destinations', function (req, res) {
-    if (req.authInfo.checkScope('$XSAPPNAME.User')) {
-        lib.getDestination(services.dest, req.authInfo.getSubdomain(), req.query.destination).then(
+    // unsubscribe/offboard a subscriber tenant
+    app.delete('/callback/v1.0/tenants/*', function (req, res) {
+        let tenantHost = req.body.subscribedSubdomain + '-' + appEnv.app.space_name.toLowerCase().replace(/_/g, '-') + '-' + services.registry.appName.toLowerCase().replace(/_/g, '-');
+        console.log('Unsubscribe: ', req.body.subscribedSubdomain, req.body.subscribedTenantId, tenantHost);
+        lib.deleteRoute(tenantHost, services.registry.appName).then(
             function (result) {
-                // result contains the destination information for use in REST calls
-                res.status(200).json(result);
+                res.status(200).send('');
             },
             function (err) {
                 console.log(err.stack);
                 res.status(500).send(err.message);
             });
-    } else {
-        res.status(403).send('Forbidden');
-    }
+    });
+
+    // get reuse service dependencies
+    app.get('/callback/v1.0/dependencies', function (req, res) {
+        let tenantId = req.params.tenantId;
+        let dependencies = [{
+            'xsappname': services.dest.xsappname
+        }];
+        console.log('Dependencies: ', tenantId, dependencies);
+        res.status(200).json(dependencies);
+    });
+
+    // app user info
+    app.get('/srv/info', function (req, res) {
+        if (req.authInfo.checkScope('$XSAPPNAME.User')) {
+            let info = {
+                'userInfo': req.user,
+                'subdomain': req.authInfo.getSubdomain(),
+                'tenantId': req.authInfo.getZoneId()
+            };
+            res.status(200).json(info);
+        } else {
+            res.status(403).send('Forbidden');
+        }
+    });
+
+    // app subscriptions
+    app.get('/srv/subscriptions', function (req, res) {
+        if (req.authInfo.checkScope('$XSAPPNAME.Administrator')) {
+            lib.getSubscriptions(services.registry).then(
+                function (result) {
+                    res.status(200).json(result);
+                },
+                function (err) {
+                    console.log(err.stack);
+                    res.status(500).send(err.message);
+                });
+        } else {
+            res.status(403).send('Forbidden');
+        }
+    });
+
+
+    // destination reuse service
+    app.get('/srv/destinations', function (req, res) {
+        if (req.authInfo.checkScope('$XSAPPNAME.User')) {
+            lib.getDestination(services.dest, req.authInfo.getSubdomain(), req.query.destination).then(
+                function (result) {
+                    // result contains the destination information for use in REST calls
+                    res.status(200).json(result);
+                },
+                function (err) {
+                    console.log(err.stack);
+                    res.status(500).send(err.message);
+                });
+        } else {
+            res.status(403).send('Forbidden');
+        }
+    });
+
+    const port = process.env.PORT || 4004;
+    app.listen(port, function () {
+        console.info('Listening on http://localhost:' + port);
+    });
 });
 
-const port = process.env.PORT || 5001;
-app.listen(port, function () {
-    console.info('Listening on http://localhost:' + port);
+cds.on('served', (app) => {
+    // add more middleware after all CDS services
 });
+
+module.exports = cds.server //> delegate to default server.js
